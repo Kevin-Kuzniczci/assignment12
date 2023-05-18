e05f5c3c9b7be937dce0fab0f50d587a5a40cf76
Kevin-Kuzniczci <60890825+Kevin-Kuzniczci@users.noreply.github.com>
2023-05-17 17:56:33 -0700

    Other than one thing, things are working fine!

--- js/player.js
+++ js/player.js
@@ -1,57 +1,87 @@
 class Jukebox {
-    constructor(albums) {
-        this.albums = []
+    constructor() {
+        this.albums = [];
     }
     addAlbum = function(album) {
-        this.albums.push(album)
+        this.albums.push(album);
     }
     favoriteAlbum = function() {
-        let max = -1, fav
+        let max = -1;
+        let fav;
         for (let i = 0; i < this.albums.length; i++) {
             if (this.albums[i].played > max) {
-                max = this.albums[i].played
-                fav = this.albums[i]
+                max = this.albums[i].played;
+                fav = this.albums[i];
             }
         }
-        return fav.display()
+        return fav.display();
     }
 }
 
 class Album {
     constructor(artist, title) {
-        this.artist = artist
-        this.title = title
-        this.played = 0
+        this.artist = artist;
+        this.title = title;
+        this.played = 0;
     }
-    play = function() {
-        this.played += 1
+    play() {
+        this.played += 1;
     }
-    display = function() {
-        return `${this.artist} : ${this.title}. The album has been played ${this.played} times.`
+    display() {
+        return `${this.artist} : ${this.title}. The album has been played ${this.played} times.`;
     }
 }
 
-var jbox = new Jukebox()
-const album1 = new Album('Operation Ivy', 'Energy')
-const album2 = new Album('Blink 182', 'Dude Ranch')
-const album3 = new Album('New Found Glory', 'Sticks and Stones')
+const albumsArray = [
+    {artist: "Operation Ivy", title: "Energy"},
+    {artist: "Blink 182", title: "Dude Ranch"},
+    {artist: "New Found Glory", title: "Sticks and Stones"}
+];
 
-jbox.addAlbum(album1)
-jbox.addAlbum(album2)
-jbox.addAlbum(album3)
+const jbox = new Jukebox()
 
-album1.play()
-album2.play()
-album2.play()
-album2.play()
-album2.play()
-album2.play()
-album3.play()
+albumsArray.forEach(album => {
+    const albumInstance = new Album(album.artist, album.title);
+    jbox.addAlbum(albumInstance);
+});
 
-const albumDropdown = document.getElementById('albumDropdown');
-const play = document.getElementById('play');
-const showFavorite = document.getElementById('showFavorite');
-const favoriteAlbum = document.getElementById('favoriteAlbum');
+document.addEventListener("DOMContentLoaded", () => {
+const albumDropdown = document.getElementById("albumDropdown");
+const playButton = document.getElementById("playButton");
+const showFavorite = document.getElementById("showFavorite");
+const favoriteAlbum = document.getElementById("favoriteAlbum");
 
+albumsArray.forEach(album => {
+    const option = document.createElement("option");
+    option.text = `${album.artist} - ${album.title}`;
+    albumDropdown.appendChild(option);
+});
 
-console.log(`Your favorite album is: ${jbox.favoriteAlbum()}`)
+playButton.addEventListener("click", () => {
+    const selectedAlbumTitle = albumDropdown.value;
+    const selectedAlbum = jbox.albums.find(album => album.title === selectedAlbumTitle);
+
+    if (selectedAlbum) {
+        selectedAlbum.play();
+    }
+});
+
+showFavorite.addEventListener("click", () => {
+    const favAlbum = jbox.favoriteAlbum();
+    favoriteAlbum.textContent = `Your favorite album is ${favAlbum}`;
+});
+});
+
+// jbox.addAlbum(album1)
+// jbox.addAlbum(album2)
+// jbox.addAlbum(album3)
+
+// album1.play()
+// album2.play()
+// album2.play()
+// album2.play()
+// album2.play()
+// album2.play()
+// album3.play()
+
+// console.log(`Your favorite album is: ${jbox.favoriteAlbum()}`)
